b93be70ec3841b5757e47cc0665fd479d3b54293
Jha Nilbodee <jhanilbodee@Pornwipa-Nilbodee.local>
2023-09-18 14:54:38 +0700

    update response

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, requests
 from utils import create_chain
 
 
@@ -17,10 +17,29 @@ def chat():
     try:
         data = request.json
         message = data["events"][0]["message"]["text"]
+        replyToken = data["events"][0]["replyToken"]
         chatgpt_chain = create_chain()
         prediction = chatgpt_chain.predict(human_input=message)
+
+        response = jsonify({
+            {
+                "messages":
+                [
+                    { "type": "text", 
+                      "text": prediction
+                      }
+                    ],
+                "replyToken": replyToken 
+                }
+        }
+        )
+
+        headers = {
+            "Authorization": "Bearer NOoNbXljlg6p2+kp3qf37iEzCokeZNxIa2X2A2MGZS2n0gjvnClnzJsqj0YhiYra8IepUAd7oGQeotFmqErxkTzisHTg44xlTAn8qbZOXaMKs1bxkCAky2uUnge5gOLnK6GI1/LHqsS0cxq0M3BecwdB04t89/1O/w1cDnyilFU="
+        }
+
+        requests.post("https://api.line.me/v2/bot/message/reply", data=response, headers=headers)
         
-        return jsonify({"messages": [{"type": "text", "text": prediction}], "replyToken": data["events"][0]["replyToken"]})
     except:
         return make_response(
             jsonify({"messages": [{"type": "text", "text": f"Unexpected error: failed to send the message ({request.json})"}], "replyToken": data["events"][0]["replyToken"]}))
